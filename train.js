f617c638cad814cdec2b404680b29af4dd7d7a48
sharitacumm <emandraine@yahoo.com>
2019-01-31 09:28:45 -0500

    Changes

--- train.js
+++ train.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-   
-   
+$(document).ready(function () {
+
+
     var config = {
         apiKey: "AIzaSyDyViFKUXvEOxaU75VLsAEYANX_9MrIgBk",
         authDomain: "train-homework-bcb28.firebaseapp.com",
@@ -8,17 +8,20 @@ $(document).ready(function() {
         projectId: "train-homework-bcb28",
         storageBucket: "train-homework-bcb28.appspot.com",
         messagingSenderId: "521877757034"
-      };
-      firebase.initializeApp(config);
-        console.log("hello file");
+    };
+    firebase.initializeApp(config);
+    console.log("hello file");
     var db = firebase.database();
-    $("#add-train-btn").on("click",function(){
-    db.ref().push({name:"test"});
-    console.log("we just clicked the button");
 
-})
+    $("#add-train-btn").on("click", function () {
+        db.ref().push({
+            name: $("#train-name-input").val().trim(),
+            destination: $("#train-name-input").val().trim(),
+            startTime: $("#train-name-input").val().trim(),
+            frequency: $("#train-name-input").val().trim()
+        });
+        console.log("we just clicked the button");
+    });
 
-
-
-}) 
+});
 
